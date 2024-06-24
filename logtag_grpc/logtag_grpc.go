7476899ea828ac9e04e9afb4cdf9e460f6ad7602
Christian Kartnig <36508191+re-ckart@users.noreply.github.com>
2024-06-24 09:20:08 +0200

    added log payload flag and truncating payload after 500 chars for safety (#12)

--- logtag_grpc/logtag_grpc.go
+++ logtag_grpc/logtag_grpc.go
@@ -8,45 +8,59 @@ import (
 	"google.golang.org/grpc"
 )
 
-func GrpcLogTagServerUnaryInterceptor(logTag string) grpc.UnaryServerInterceptor {
+func GrpcLogTagServerUnaryInterceptor(logTag string, logPayload bool) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 
-		logtag.Printf(logTag, "↘️ %s: %s", info.FullMethod, req)
+		if logPayload {
+			logtag.Printf(logTag, "↘️ %s: %.500s", info.FullMethod, req)
+		} else {
+			logtag.Printf(logTag, "↘️ %s: <payload truncated>", info.FullMethod)
+		}
 		// Calls the handler
 		h, err := handler(ctx, req)
 
 		if err != nil {
 			logtag.Errorf(logTag, "↗️ %s: %s", info.FullMethod, logtag.ToColoredText(logtag.Red, err.Error()))
+		} else if logPayload {
+			logtag.Printf(logTag, "↘️ %s: %.500s", info.FullMethod, h)
 		} else {
-			logtag.Printf(logTag, "↗️ %s: %s", info.FullMethod, h)
+			logtag.Printf(logTag, "↘️ %s: <payload truncated>", info.FullMethod)
 		}
 
 		return h, err
 	}
 }
 
-func GrpcLogTagClientUnaryInterceptor(logTag string) grpc.UnaryClientInterceptor {
+func GrpcLogTagClientUnaryInterceptor(logTag string, logPayload bool) grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req interface{}, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-		logtag.Printf(logTag, "↗️ %s: %s", method, req)
+
+		if logPayload {
+			logtag.Printf(logTag, "↗️ %s: %.500s", method, req)
+		} else {
+			logtag.Printf(logTag, "↗️ %s: <payload truncated>", method)
+		}
+
 		// Calls the handler
 		err := invoker(ctx, method, req, reply, cc, opts...)
 
 		if err != nil {
 			logtag.Errorf(logTag, "↘️ %s: %s", method, logtag.ToColoredText(logtag.Red, err.Error()))
+		} else if logPayload {
+			logtag.Printf(logTag, "↘️ %s: %.500s", method, reply)
 		} else {
-			logtag.Printf(logTag, "↘️ %s: %s", method, reply)
+			logtag.Printf(logTag, "↘️ %s: <payload truncated>", method)
 		}
 
 		return err
 	}
 }
 
-func GrpcLogTagServerStreamInterceptor(logTag string) grpc.StreamServerInterceptor {
+func GrpcLogTagServerStreamInterceptor(logTag string, logPayload bool) grpc.StreamServerInterceptor {
 	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 
 		logtag.Printf(logTag, "↘️ %s: streaming started (client streaming: %t, server streaming: %t)", info.FullMethod, info.IsClientStream, info.IsServerStream)
 		// Calls the handler
-		err := handler(srv, &serverStreamMsgInterceptor{ServerStream: ss, tag: logTag, info: info})
+		err := handler(srv, &serverStreamMsgInterceptor{ServerStream: ss, tag: logTag, info: info, logPayload: logPayload})
 		if err == io.EOF {
 			return err
 		}
@@ -62,8 +76,9 @@ func GrpcLogTagServerStreamInterceptor(logTag string) grpc.StreamServerIntercept
 
 type serverStreamMsgInterceptor struct {
 	grpc.ServerStream
-	tag  string
-	info *grpc.StreamServerInfo
+	tag        string
+	info       *grpc.StreamServerInfo
+	logPayload bool
 }
 
 func (s *serverStreamMsgInterceptor) SendMsg(m any) error {
@@ -74,7 +89,12 @@ func (s *serverStreamMsgInterceptor) SendMsg(m any) error {
 	if err != nil {
 		logtag.Errorf(s.tag, "↗️ %s: %s", s.info.FullMethod, logtag.ToColoredText(logtag.Red, err.Error()))
 	} else if s.info.IsServerStream {
-		logtag.Printf(s.tag, "↗️ %s: %s", s.info.FullMethod, m)
+		if s.logPayload {
+			logtag.Printf(s.tag, "↗️ %s: %.500s", s.info.FullMethod, m)
+		} else {
+			logtag.Printf(s.tag, "↗️ %s: <payload truncated>", s.info.FullMethod)
+		}
+
 	}
 
 	return err
@@ -88,14 +108,18 @@ func (s *serverStreamMsgInterceptor) RecvMsg(m any) error {
 	if err != nil {
 		logtag.Errorf(s.tag, "↘️ %s: %s", s.info.FullMethod, logtag.ToColoredText(logtag.Red, err.Error()))
 	} else if s.info.IsClientStream {
-		logtag.Printf(s.tag, "↘️ %s: %s", s.info.FullMethod, m)
+		if s.logPayload {
+			logtag.Printf(s.tag, "↘️ %s: %.500s", s.info.FullMethod, m)
+		} else {
+			logtag.Printf(s.tag, "↘️ %s: <payload truncated>", s.info.FullMethod)
+		}
 	}
 
 	return err
 }
 
 // TODO GrpcLogTagClientStreamInterceptor Not tested, might need some love
-func GrpcLogTagClientStreamInterceptor(logTag string) grpc.StreamClientInterceptor {
+func GrpcLogTagClientStreamInterceptor(logTag string, logPayload bool) grpc.StreamClientInterceptor {
 	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
 
 		logtag.Printf(logTag, "↘️ %s: streaming started  (client streaming: %t, server streaming: %t)", method, desc.ClientStreams, desc.ServerStreams)
@@ -109,15 +133,16 @@ func GrpcLogTagClientStreamInterceptor(logTag string) grpc.StreamClientIntercept
 		} else {
 			logtag.Printf(logTag, "↗️ %s: streaming closed", method)
 		}
-		return &clientStreamMsgInterceptor{ClientStream: clientStream, tag: logTag, desc: desc, method: method}, nil
+		return &clientStreamMsgInterceptor{ClientStream: clientStream, tag: logTag, desc: desc, method: method, logPayload: logPayload}, nil
 	}
 }
 
 type clientStreamMsgInterceptor struct {
 	grpc.ClientStream
-	desc   *grpc.StreamDesc
-	tag    string
-	method string
+	desc       *grpc.StreamDesc
+	tag        string
+	method     string
+	logPayload bool
 }
 
 func (c *clientStreamMsgInterceptor) SendMsg(m any) error {
@@ -126,7 +151,11 @@ func (c *clientStreamMsgInterceptor) SendMsg(m any) error {
 	if err != nil {
 		logtag.Errorf(c.tag, "%s: %s", c.method, logtag.ToColoredText(logtag.Red, err.Error()))
 	} else if c.desc.ClientStreams {
-		logtag.Printf(c.tag, "↗️ %s: %s", c.method, m)
+		if c.logPayload {
+			logtag.Printf(c.tag, "↗️ %s: %s", c.method, m)
+		} else {
+			logtag.Printf(c.tag, "↗️ %s: <payload truncated>", c.method)
+		}
 	}
 
 	return err
@@ -138,7 +167,11 @@ func (c *clientStreamMsgInterceptor) RecvMsg(m any) error {
 	if err != nil && err != io.EOF {
 		logtag.Errorf(c.tag, "%s: %s", c.method, logtag.ToColoredText(logtag.Red, err.Error()))
 	} else if c.desc.ServerStreams {
-		logtag.Printf(c.tag, "↘️ %s: %s", c.method, m)
+		if c.logPayload {
+			logtag.Printf(c.tag, "↘️ %s: %s", c.method, m)
+		} else {
+			logtag.Printf(c.tag, "↘️ %s: <payload truncated>", c.method)
+		}
 	}
 
 	return err
